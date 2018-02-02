d9961226461eb57739689a62c4c998c53ab7a9c8
robdir <rhwickham@gmail.com>
2018-02-02 13:32:38 +0100

    index setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3030
+
+let app = express()
+
+app.get('/', (req, res) => {
+    res.send('Whattup')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port${PORT}`)
+})
